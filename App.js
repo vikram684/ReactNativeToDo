9732b7b1a001b36f28ae36aa15309f4efd02ed0d
vikram684 <65454629+vikram684@users.noreply.github.com>
2022-01-09 09:51:20 +0530

    Fix: nameing convention

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const title = 'Todo';
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>Start a new {title}</Text>
+        <TextInput
+          style={{
+            height:40,
+            width:380,
+            borderWidth:2
+          }}
+          defaultValue='Type here...'
+        />
+      <Button
+        onPress={() => {
+          alert('You tapped the button!');
+        }}
+        title="Press Me"
+      />
+        <View style={{
+            width: 50, height: 50,marginTop:100, backgroundColor: 'steelblue'
+          }} />
     </View>
   );
 }
@@ -13,8 +30,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontSize:200,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});p
